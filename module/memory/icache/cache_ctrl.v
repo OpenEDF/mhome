07c91295258b45359697c5be2786e5c7c0c1410c
macro <makermuyi@163.com>
2023-07-07 08:58:04 +0800

    update cache ctrl fsm

--- module/memory/icache/cache_ctrl.v
+++ module/memory/icache/cache_ctrl.v
@@ -27,7 +27,15 @@
 
 //--------------------------------------------------------------------------
 // Designer: macro
-// Brief:
+// Brief: 8KB instruction cache, 4-way set asssociative, 1 word block size
+//        number of block: 8K / 4 = 2048 block
+//        number of set:   2048 / 4 = 512
+//        Tag:    21 bit
+//        set:    9  bit
+//        offset: 2  bit
+//        ditry:  1  bit
+//        vaild:  1  bit
+//        lru:    1  bit
 // Change Log:
 //--------------------------------------------------------------------------
 
@@ -47,13 +55,13 @@ module cache_ctrl
     // inputs
     input wire         clk,
     input wire         rst_n,
-    input wire  [31:0] cpu_if_pc_cache,
-    input wire         cpu_write_en_cache,
-    input wire         cpu_read_en_cache,
+    input wire  [31:0] cpu_addr_cache,
+    input wire         cpu_write_req_cache,
+    input wire         cpu_read_req_cache,
 
     // outputs
-    output wire        cache_miss_cpu,
-    output wire [31:0] cache_inst_data_cpu
+    output reg         cache_stall_cpu,
+    output reg  [31:0] cache_inst_data_cpu
 );
 
 //--------------------------------------------------------------------------
@@ -67,6 +75,7 @@ localparam READ_CACHE     = 8'b0001_0000;
 localparam READ_MAIN_MEM  = 8'b0010_0000;
 localparam BRING_DATA     = 8'b0100_0000;
 localparam RET_TO_CPU     = 8'b1000_0000;
+reg hit_miss;
 
 //--------------------------------------------------------------------------
 // Design: cache FSM, one always sequential logic register output
@@ -74,29 +83,50 @@ localparam RET_TO_CPU     = 8'b1000_0000;
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         cur_state  <= RESET;
+        cache_stall_cpu <= 1'b0;
     end else begin
-        cur_state  <= REQ_FROM_CPU;
+        cur_state  <= IDLE;
+        cache_stall_cpu <= 1'b0;
         case(cache_state)
-            REQ_FROM_CPU: begin
+            IDLE: begin
                 /* compare the cpu address tag and cache line tag */
-                if (write_hit) begin
-                    cur_state <= WRITE_CACHE;
+                hit_miss = (way1_vaild[set] && (way1_tag[set] == tag))
+                if (!hit_miss) begin /* miss */
+                    cur_state <= READ_MAIN_MEM;
+                end else begin /* hit */
+                    if (read_req) begin
+                        cur_state <= READ_CACHE;
+                    end else if (write_req) begin
+                        cur_state <= WRITE_CACHE;
+                    end else begin
+                        cur_state <= IDLE;
+                    end
                 end
             end
-            WRITE_CACHE:
-            WRITE_MAIN_MEM:
-            READ_CACHE:
             READ_MAIN_MEM:
+                cur_state <= BRING_DATA;
+            READ_CACHE:
+                cur_state <= RET_DATA;
+            WRITE_CACHE:
+                if (dirty)
+                    cur_state <= WRITE_BACK_MEM;
+                else
+                    cur_state <= IDLE;
+            WRITE_BACK_MEM:  /* write old value to main memory */
+                cur_state <= IDLE;
             BRING_DATA:
-            RET_TO_CPU:
+                if (read_req) begin
+                    cur_state <= READ_CACHE;
+                end else if begin
+                    cur_state <= WRITE_CACHE;
+                end
+            RET_DATA:
+                cur_state <= IDLE;
             default:
+                cur_state <= IDLE;
         endcase
     end
 end
-
-//--------------------------------------------------------------------------
-// Design: compare the cpu address tag and cache line tag
-//--------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------
 // Design: cache write data

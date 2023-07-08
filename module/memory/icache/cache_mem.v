89a28491a7afaaf0c708777713a3cb1661035eb3
macro <makermuyi@163.com>
2023-07-08 08:01:01 +0800

    add the cache memory module

--- module/memory/icache/cache_mem.v
+++ module/memory/icache/cache_mem.v
@@ -34,26 +34,104 @@
 //--------------------------------------------------------------------------
 // Include File
 //--------------------------------------------------------------------------
-`include "mhome_defines.v"
+//`include "cache_defines.v"
 
 //--------------------------------------------------------------------------
 // Module
 //--------------------------------------------------------------------------
-module cache_mem
+module cache_mem #(
+    parameter BLOCK_WIDTH = 32,
+    parameter SET_SIZE    = 512,
+    parameter SET_WIDTH   = 9,
+    parameter TAG_WIDTH   = 21
+)
 //--------------------------------------------------------------------------
 // Ports
 //--------------------------------------------------------------------------
 (
     // inputs
-    input wire         clk,
-    input wire         rst_n,
+    input wire [SET_WIDTH-1:0]    set_addr,
+    input wire                    wr_block_en,
+    input wire                    wr_block_data,
+    input wire                    wr_dirty_en,
+    input wire                    wr_dirty_data,
+    input wire [TAG_WIDTH-1:0]    wr_tag_en,
+    input wire                    wr_tag_data,
+    input wire                    wr_vaild_en,
+    input wire                    wr_vaild_data,
+    input wire                    wr_lru_en,
+    input wire                    wr_lru_data,
 
     // outputs
+    output wire [BLOCK_WIDTH-1:0] block_data,
+    output wire [TAG_WIDTH-1:0]   tag_data,
+    output wire                   dirty_data,
+    output wire                   vaild_data,
+    output wire                   lru_data
 );
 
 //--------------------------------------------------------------------------
-// Design:
+// Design: memory descript
 //--------------------------------------------------------------------------
+reg [BLOCK_WITDH-1:0] block[SET_SIZE];
+reg                   dirty[SET_SIZE];
+reg                   vaild[SET_SIZE];
+reg                   lru[SET_SIZE];
+reg [TAG_WIDTH-1:0]   tag[SET_SIZE];
+
+//--------------------------------------------------------------------------
+// Design: write memory
+//--------------------------------------------------------------------------
+always @(wr_block_en or
+        wr_dirty_en or
+        wr_tag_en or
+        wr_vaild_en or
+        wr_lru_en) begin
+    if (wr_block_en) begin
+        block[set_addr] <= wr_block_data;
+    end else if (wr_tag_en) begin
+        tag[set_addr]   <= wr_tag_data;
+    end else if (wr_dirty_en) begin
+        dirty[set_addr] <= wr_dirty_data;
+    end else if (wr_vaild_en) begin
+        vaild[set_addr] <= wr_vaild_data;
+    end else if (wr_lru_en) begin
+        lru[set_addr]   <= wr_lru_data;
+    end else begin
+        block[set_addr] <= block[set_addr];
+        tag[set_addr]   <= tag[set_addr];
+        dirty[set_addr] <= dirty[set_addr];
+        vaild[set_addr] <= vaild[set_addr];
+        lru[set_addr]   <= lru[set_addr];
+    end
+end
+
+//--------------------------------------------------------------------------
+// Design: read memory
+//--------------------------------------------------------------------------
+always @(wr_block_en or
+        wr_dirty_en or
+        wr_tag_en or
+        wr_vaild_en or
+        wr_lru_en) begin
+    if (~wr_block_en) begin
+        block_data <= block[set_addr];
+    end else if (~wr_tag_en) begin
+        tag_data   <= tag[set_addr];
+    end else if (~wr_dirty_en) begin
+        dirty_data <= dirty[set_addr];
+    end else if (~wr_vaild_en) begin
+        vaild_data <= vaild[set_addr];
+    end else if (~wr_lru_en) begin
+        lru_data   <= lru[set_addr];
+    end else begin
+        block_data <= BLOCK_WITDH'b0;
+        tag_data   <= TAG_WIDTH'b0;
+        dirty_data <= 1'b0;
+        vaild_data <= 1'b0;
+        lru_data   <= 1'b0;
+    end
+end
 
 endmodule
 //--------------------------------------------------------------------------

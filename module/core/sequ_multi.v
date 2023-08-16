1378c985c16605578e9c557b34492f210065958a
macro <makermuyi@163.com>
2023-08-16 10:31:12 +0800

    update the multipler module

--- module/core/sequ_multi.v
+++ module/core/sequ_multi.v
@@ -53,7 +53,6 @@
 //--------------------------------------------------------------------------
 // Include File
 //--------------------------------------------------------------------------
-`include "mhome_defines.v"
 
 //--------------------------------------------------------------------------
 // Module
@@ -71,48 +70,130 @@ module sequ_multi
     input wire         start,
 
     // outputs
-    output reg [31:0]  product,
+    output reg [31:0]  product_l,
+    output reg [31:0]  product_h,
     output reg         ready
 );
 
 //--------------------------------------------------------------------------
-// Design:temp register 
+// Design:  FSM state one hot design
 //--------------------------------------------------------------------------
-reg [63:0] partial_product;
-reg [31:0] multipiler_copy;
-reg [63:0] multipilcand_copy;
-reg [5:0]  shift_count;
+localparam MULTI_IDLE          = 7'b0000001;
+localparam MULTI_START         = 7'b0000010;
+localparam MULTI_ER_CAND_ZERO  = 7'b0000100;
+localparam MULTI_ER_ONE        = 7'b0001000;
+localparam MULTI_CAND_ONE      = 7'b0010000;
+localparam MULTI_CALCUL        = 7'b0100000;
+localparam MULTI_OUTPUT        = 7'b1000000;
 
 //--------------------------------------------------------------------------
-// Design: sequential multiplier shift version of multiplier and 
+// Design:temp register
+//--------------------------------------------------------------------------
+reg [63:0] partial_product;
+reg [31:0] multiplier_copy;
+reg [63:0] multiplicand_copy;
+reg [5:0]  shift_count;
+reg        lsb;
+reg [6:0]  state;
+
+//--------------------------------------------------------------------------
+// Design: sequential multiplier shift version of multiplier and
 //         multoplicand
 //--------------------------------------------------------------------------
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        product           <= 32'h0000_0000;
+        product_l         <= 32'h0000_0000;
+        product_h         <= 32'h0000_0000;
         ready             <= 1'b0;
         partial_product   <= 64'h0000_0000_0000_0000;
         multiplier_copy   <= 32'h0000_0000;
-        multipilcand_copy <= 32'h0000_0000;
-        shift_count       <= 6'd32;
+        multiplicand_copy <= 64'h0000_0000;
+        shift_count       <= 6'd0;
+        lsb               = 1'b0;
+        state             <= MULTI_IDLE;
     end else begin
-        if (start || ~ready) begin
-            multipilcand_copy <= {32'h0000_0000, multoplicand};
-            multiplier_copy   <= multiplier;
-            shift_count       <= 6'd32;
-            ready   <= 1'b1;
-        end else if (shift_count == 5'b00000) begin
-            product <= partial_product[31:0];    
-            ready   <= 1'b0;
-        end else begin
-            if (multiplier_copy[0] == 1'b1) begin
-                partial_product <= partial_product + multipilcand_copy;
+        /* default output */
+        product_l         <= 32'h0000_0000;
+        product_h         <= 32'h0000_0000;
+        ready             <= 1'b0;
+        case (state)
+            MULTI_IDLE: begin
+                if (start) begin
+                    state <= MULTI_START;
+                end else begin
+                    /* update state */
+                    state <= MULTI_IDLE;
+                end
             end
-            multipilcand_copy <= multipilcand_copy << 1;
-            multiplier_copy <= multiplier_copy >> 1;
-            shift_count     <= shift_count - 1;
-        end
+            MULTI_START: begin
+                multiplicand_copy <= {32'h0000_0000, multiplicand};
+                multiplier_copy   <= multiplier;
+                shift_count       <= 6'd32;
+                ready             <= 1'b0;
+                partial_product   <= 64'h0000_0000_0000_0000;
+                /* update state */
+                state <= MULTI_ER_CAND_ZERO;
+            end
+            MULTI_ER_CAND_ZERO: begin
+                if ((multiplier == 32'h0000_0000) || (multiplicand == 32'h0000_0000)) begin
+                    partial_product   <= 64'h0000_0000_0000_0000;
+                    /* update state */
+                    state <= MULTI_OUTPUT;
+                end else begin
+                    /* update state */
+                    state <= MULTI_ER_ONE;
+                end
+            end
+            MULTI_ER_ONE: begin
+                if (multiplier == 32'h0000_0001) begin
+                    partial_product   <= {32'h0000_0000, multiplicand};
+                    /* update state */
+                    state <= MULTI_OUTPUT;
+                end else begin
+                    /* update state */
+                    state <= MULTI_CAND_ONE;
+                end
+            end
+            MULTI_CAND_ONE: begin
+                if (multiplicand == 32'h0000_0001) begin
+                    partial_product   <= {32'h0000_0000, multiplier};
+                    /* update state */
+                    state <= MULTI_OUTPUT;
+                end else begin
+                    /* update state */
+                    state <= MULTI_CALCUL;
+                end
+            end
+            MULTI_CALCUL: begin
+                if (shift_count == 6'b000000) begin
+                    /* update state */
+                    state <= MULTI_OUTPUT;
+                end else begin
+                    lsb = multiplier_copy[0];
+                    if (lsb == 1'b1) begin
+                        partial_product <= partial_product + multiplicand_copy;
+                    end
+                    multiplier_copy   <= multiplier_copy >> 1;
+                    multiplicand_copy <= multiplicand_copy << 1;
+                    shift_count       <= shift_count - 1;
+                    /* update state */
+                    state <= MULTI_CALCUL;
+                end
+            end
+            MULTI_OUTPUT: begin
+                product_l <= partial_product[31:0];
+                product_h <= partial_product[63:32];
+                ready     <= 1'b1;
+                /* update state */
+                state <= MULTI_IDLE;
+            end
+            default: begin
+                /* update state */
+                state <= MULTI_IDLE;
+            end
+        endcase
     end
 end
+
 endmodule
 //--------------------------------------------------------------------------

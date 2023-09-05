cd37df48811b460ef54dc7aab44f05a608a5351f
macro <makermuyi@163.com>
2023-09-05 23:12:31 +0800

    add conter module

---
+++ module/utils/counter.v
@@ -0,0 +1,77 @@
+//--------------------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: counter
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+module counter 
+#(
+    parameter DATA_WIDTH = 32
+)
+//--------------------------------------------------------------------------
+// Ports
+//--------------------------------------------------------------------------
+(
+    // inputs
+    input wire                  clk,
+    input wire                  rst_n,
+    input wire [DATA_WIDTH-1:0] pre_data,
+    input wire                  clear,
+    input wire                  pre_load,
+    input wire                  up_down,
+
+    // outputs
+    output reg [DATA_WIDTH-1:0] count
+);
+
+//--------------------------------------------------------------------------
+// Design: counter
+//--------------------------------------------------------------------------
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n || clear) begin
+        count <= 0;
+    end else if (pre_load) begin
+        count <= pre_data;
+    end else if (up_down) begin
+        count <= count + 1;
+    end else begin
+        count <= count - 1;
+    end
+end
+
+endmodule
+//--------------------------------------------------------------------------
